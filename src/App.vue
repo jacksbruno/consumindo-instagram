9ec192c57b4300702219c9eeedf50b6031b9827b
Jacks Bruno <jacksbruno22@gmail.com>
2021-05-06 18:37:48 -0400

    Removendo comentario automatico

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,6 @@
 <template>
   <div id="app">
-    <ListaFotos msg="Welcome to Your Vue.js App"/>
+    <ListaFotos />
   </div>
 </template>
 
--- src/components/ListaFotos.vue
+++ src/components/ListaFotos.vue
@@ -20,7 +20,7 @@ export default {
   methods: {
     /* Busca Albuns */
     async buscaAlbumFotos(){
-      let resp = await this.$axios.get('https://graph.instagram.com/me/media?fields=id,caption&access_token=IGQVJXYTdVcUg4LVd5c3l6TEdxYnpEcEdyU2pZAYU44QlRJZAHk5LVFmSzFvU0ZAVS3VTSUJCTUtSWFBqcFBINEc3RHF0Y1FSTURhM0oxZAXZA0TGZAoM0RZARmdzc05lTHZABdGNYWnIyeXN5YWVoa2R6OG5tMgZDZD')
+      let resp = await this.$axios.get('https://graph.instagram.com/me/media?fields=id,caption&access_token=TOKEN-GERADO')
 
       this.dadosAlbuns = resp.data.data.slice(0,4)
 
@@ -31,7 +31,7 @@ export default {
     },
     /* Busca fotos */
     async capturaFotos(id){
-      let resp = await this.$axios.get(`https://graph.instagram.com/${id}?fields=id,media_type,media_url,username,timestamp&access_token=IGQVJXYTdVcUg4LVd5c3l6TEdxYnpEcEdyU2pZAYU44QlRJZAHk5LVFmSzFvU0ZAVS3VTSUJCTUtSWFBqcFBINEc3RHF0Y1FSTURhM0oxZAXZA0TGZAoM0RZARmdzc05lTHZABdGNYWnIyeXN5YWVoa2R6OG5tMgZDZD`)
+      let resp = await this.$axios.get(`https://graph.instagram.com/${id}?fields=id,media_type,media_url,username,timestamp&access_token=TOKEN-GERADO`)
 
       this.fotos.push(resp.data.media_url)
 
